9d04a077056b9e7269d6b7162d8d2b5d20827030
GitHubArvind01 <kumararvind21390@gmail.com>
2026-04-17 16:48:40 +0530

    [ARVIND] : Updated the serivce layer interface for business contract

---
+++ microservices/auth-service/src/main/java/com/resumeai/auth/service/UserService.java
@@ -0,0 +1,42 @@
+package com.resumeai.auth.service;
+
+import com.resumeai.auth.dtos.AuthResponse;
+import com.resumeai.auth.dtos.LoginRequest;
+import com.resumeai.auth.dtos.RegisterRequest;
+import com.resumeai.auth.dtos.UpdateProfileRequest;
+import com.resumeai.auth.dtos.UserResponseDTO;
+
+public interface UserService {
+
+    /*
+     * AUTH - REGISTRATION
+     */
+    String registerRequest(RegisterRequest registerRequest);
+
+    AuthResponse registerUser(String email, String otp);
+
+    /*
+     * AUTH - LOGIN
+     */
+    AuthResponse loginUser(LoginRequest loginRequest);
+
+    /*
+     * FORGOT PASSWORD FLOW
+     */
+    String initiateForgetPassword(String email);
+
+    String verifyOtp(String email, String otp);
+
+    String resetPassword(String email, String newPassword);
+    
+    /*
+     * Update User
+     */
+    void updateSubscription(String email, String plan);
+    
+    UserResponseDTO updateProfile(String email, UpdateProfileRequest updateUser);
+    
+    public String verifyEmailUpdate(String currentEmail, String otp);
+    
+    public UserResponseDTO getUserByEmail(String email);
+}

cc792cabb5c1bbea36a9c9ba03728b0c7d3d14a9
GitHubArvind01 <kumararvind21390@gmail.com>
2026-04-18 10:42:26 +0530

    [ARVIND] : Added the inital setup for porject

---
+++ microservices/resume-section-service/src/main/java/com/resumeai/resume_section_service/ResumeSectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.resumeai.resume_section_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeSectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeSectionServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/resume-section-service/src/test/java/com/resumeai/resume_section_service/ResumeSectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumeai.resume_section_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeSectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

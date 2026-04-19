a0ce5ee5b39f2aaec5689c726da1198572463717
GitHubArvind01 <kumararvind21390@gmail.com>
2026-04-19 18:09:31 +0530

    [ARVIND] : Updated Section Mapper for entity-DTO conversion

--- microservices/resume-section-service/src/main/java/com/resumeai/resume_section_service/mapper/SectionMapper.java
+++ microservices/resume-section-service/src/main/java/com/resumeai/resume_section_service/mapper/SectionMapper.java
@@ -1,7 +1,7 @@
 package com.resumeai.resume_section_service.mapper;
 
-import com.resumeai.resume_section_service.repository.dto.ResumeSectionRequestDTO;
-import com.resumeai.resume_section_service.repository.dto.ResumeSectionResponseDTO;
+import com.resumeai.resume_section_service.dto.ResumeSectionRequestDTO;
+import com.resumeai.resume_section_service.dto.ResumeSectionResponseDTO;
 import com.resumeai.resume_section_service.entity.ResumeSection;
 import org.springframework.stereotype.Component;
 

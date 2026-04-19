9e72dace4ad2df2fbcfe0c6321b6e1a72768d15d
GitHubArvind01 <kumararvind21390@gmail.com>
2026-04-19 19:15:38 +0530

    [ARVIND] : Updated Resume Section Service application

--- microservices/resume-section-service/src/main/java/com/resumeai/resume_section_service/ResumeSectionServiceApplication.java
+++ microservices/resume-section-service/src/main/java/com/resumeai/resume_section_service/ResumeSectionServiceApplication.java
@@ -3,9 +3,11 @@ package com.resumeai.resume_section_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class ResumeSectionServiceApplication {
 
 	public static void main(String[] args) {

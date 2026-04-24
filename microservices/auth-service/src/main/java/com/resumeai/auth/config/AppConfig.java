a976993b1d8c5b889cc526bfae1b073f802bd7f3
GitHubArvind01 <kumararvind21390@gmail.com>
2026-04-25 01:37:45 +0530

    [ARVIND] : Updated app and security configuration for admin features

--- microservices/auth-service/src/main/java/com/resumeai/auth/config/AppConfig.java
+++ microservices/auth-service/src/main/java/com/resumeai/auth/config/AppConfig.java
@@ -2,6 +2,8 @@ package com.resumeai.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +15,13 @@ import lombok.AllArgsConstructor;
 public class AppConfig {
 	@Bean
     RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    @LoadBalanced
+    @Qualifier("loadBalancedRestTemplate")
+    RestTemplate loadBalancedRestTemplate() {
         return new RestTemplate();
     }
 	

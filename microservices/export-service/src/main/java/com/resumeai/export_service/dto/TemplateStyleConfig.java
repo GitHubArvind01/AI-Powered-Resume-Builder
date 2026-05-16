02d1361430c1d6cb7e5cbffb5d3a6ffa9ee76a0e
GitHubArvind01 <kumararvind21390@gmail.com>
2026-05-17 04:30:28 +0530

    [ARVIND] : Adjust TemplateStyleConfig property mappings for cleaner PDF generation

--- microservices/export-service/src/main/java/com/resumeai/export_service/dto/TemplateStyleConfig.java
+++ microservices/export-service/src/main/java/com/resumeai/export_service/dto/TemplateStyleConfig.java
@@ -1,8 +1,12 @@
 package com.resumeai.export_service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TemplateStyleConfig {
     private String variant;
     private String theme;
